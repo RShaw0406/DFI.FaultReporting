ed37940142f2d86d82e882befc45c031a75c54fb
Ryan Shaw <Shaw-R7@ulster.ac.uk>
2024-07-05 02:16:31 +0100

    AdminMenu added.

---
+++ DFI.FaultReporting.Admin/Pages/Admin/AdminMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DFI.FaultReporting.Admin.Pages.Admin
+{
+    public class AdminMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
