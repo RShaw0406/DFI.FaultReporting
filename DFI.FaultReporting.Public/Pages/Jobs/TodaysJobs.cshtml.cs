c95e9c0de52da039d77ae7bc3facb59c1b7ea613
Ryan Shaw <Shaw-R7@ulster.ac.uk>
2024-07-14 03:06:46 +0100

    Initial route plotting functionality

--- DFI.FaultReporting.Public/Pages/Jobs/TodaysJobs.cshtml.cs
+++ DFI.FaultReporting.Public/Pages/Jobs/TodaysJobs.cshtml.cs
@@ -1,12 +1,458 @@
+using DFI.FaultReporting.Common.Pagination;
+using DFI.FaultReporting.Common.SessionStorage;
+using DFI.FaultReporting.Interfaces.Admin;
+using DFI.FaultReporting.Interfaces.FaultReports;
+using DFI.FaultReporting.Models.Admin;
+using DFI.FaultReporting.Models.FaultReports;
+using DFI.FaultReporting.Models.Roles;
+using DFI.FaultReporting.Models.Users;
+using DFI.FaultReporting.Services.Interfaces.Admin;
+using DFI.FaultReporting.Services.Interfaces.Pagination;
+using DFI.FaultReporting.Services.Interfaces.Settings;
+using DFI.FaultReporting.Services.Interfaces.Users;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.Security.Claims;
 
 namespace DFI.FaultReporting.Public.Pages.Jobs
 {
     public class TodaysJobsModel : PageModel
     {
-        public void OnGet()
+        #region Dependency Injection
+        //Declare dependencies.
+        private readonly ILogger<TodaysJobsModel> _logger;
+        private readonly IFaultService _faultService;
+        private readonly IFaultPriorityService _faultPriorityService;
+        private readonly IFaultStatusService _faultStatusService;
+        private readonly IFaultTypeService _faultTypeService;
+        private readonly IUserService _userService;
+        private readonly IContractorService _contractorService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IPagerService _pagerService;
+        private readonly ISettingsService _settingsService;
+        private readonly IRepairService _repairService;
+        private readonly IRepairStatusService _repairStatusService;
+
+        //Inject dependencies in constructor.
+        public TodaysJobsModel(ILogger<TodaysJobsModel> logger, IFaultService faultService, IFaultTypeService faultTypeService,
+            IFaultPriorityService faultPriorityService, IFaultStatusService faultStatusService, IUserService userService, IContractorService contractorService,
+            IHttpContextAccessor httpContextAccessor, IPagerService pagerService, ISettingsService settingsService, IRepairService repairService, IRepairStatusService repairStatusService)
         {
+            _logger = logger;
+            _faultService = faultService;
+            _faultPriorityService = faultPriorityService;
+            _faultStatusService = faultStatusService;
+            _faultTypeService = faultTypeService;
+            _httpContextAccessor = httpContextAccessor;
+            _pagerService = pagerService;
+            _settingsService = settingsService;
+            _repairService = repairService;
+            _userService = userService;
+            _contractorService = contractorService;
+            _repairStatusService = repairStatusService;
         }
+        #endregion Dependency Injection
+
+        #region Properties
+        public User CurrentUser { get; set; }
+
+        [BindProperty]
+        public Contractor Contractor { get; set; }
+
+        [BindProperty]
+        public List<Contractor> Contractors { get; set; }
+
+        [BindProperty]
+        public List<Fault> Faults { get; set; }
+
+        [BindProperty]
+        public List<Repair> Repairs { get; set; }
+
+        [BindProperty]
+        public List<Repair> PagedRepairs { get; set; }
+
+        [BindProperty]
+        public List<RepairStatus> RepairStatuses { get; set; }
+
+        [BindProperty]
+        public IEnumerable<SelectListItem> RepairStatusList { get; set; }
+
+        [DisplayName("Repair status")]
+        [BindProperty]
+        public int RepairStatusFilter { get; set; }
+
+        [BindProperty]
+        [DisplayName("Target met")]
+        public int? TargetMetFilter { get; set; }
+
+        [BindProperty]
+        public List<FaultPriority> FaultPriorities { get; set; }
+
+        [BindProperty]
+        public List<FaultStatus> FaultStatuses { get; set; }
+
+        [BindProperty]
+        public List<FaultType> FaultTypes { get; set; }
+
+        [BindProperty]
+        public bool ShowRepairMap { get; set; }
+
+        [BindProperty]
+        public bool ShowRepairTable { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public Pager Pager { get; set; } = new Pager();
+
+        [BindProperty(SupportsGet = true)]
+        public SelectedJobsInputModel SelectedJobsInput { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public List<SelectedJobsInputModel> AvailableJobs { get; set; }
+
+        [BindProperty]
+        public List<SelectedJobsInputModel> SelectedJobs { get; set; }
+
+        public class SelectedJobsInputModel
+        {
+            [BindProperty]
+            public int RepairID { get; set; }
+
+            [BindProperty]
+            public bool isSelected { get; set; }
+        }
+
+        public bool ShowRouteMap { get; set; }
+        #endregion Properties
+
+        #region Page Load
+        //Method Summary:
+        //This method is executed when the page loads.
+        //When executed the user is checked for authentication and role, the required data is retrieved from the DB.
+        public async Task<IActionResult> OnGetAsync()
+        {
+            //The contexts current user exists.
+            if (_httpContextAccessor.HttpContext.User != null)
+            {
+                //The contexts current user has been authenticated.
+                if (_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated == true && HttpContext.User.IsInRole("Contractor"))
+                {
+                    //Show the repair map by default.
+                    ShowRepairMap = true;
+
+                    //Clear session and tempdata to ensure fresh start.
+                    HttpContext.Session.Clear();
+                    TempData.Clear();
+
+                    await PopulateProperties();
+
+                    //Setup Pager.
+                    Pager.CurrentPage = 1;
+                    Pager.Count = Repairs.Count;
+                    Pager.PageSize = await _settingsService.GetSettingInt(DFI.FaultReporting.Common.Constants.Settings.PAGESIZE);
+                    PagedRepairs = await _pagerService.GetPaginatedRepairs(Repairs, Pager.CurrentPage, Pager.PageSize);
+                    PagedRepairs = PagedRepairs.OrderBy(r => r.RepairTargetDate).ToList();
+
+                    //Create a new list of AvailableJobs objects.
+                    AvailableJobs = new List<SelectedJobsInputModel>();
+
+                    //Loop through all repairs and add them to the AvailableJobs list.
+                    foreach (Repair repair in PagedRepairs)
+                    {
+                        //Create a new SelectedJobsInput object.
+                        SelectedJobsInput = new SelectedJobsInputModel();
+                        SelectedJobsInput.RepairID = repair.ID;
+                        SelectedJobsInput.isSelected = false;
+
+                        //Add the SelectedJobsInput object to the AvailableJobs list.
+                        AvailableJobs.Add(SelectedJobsInput);
+                    }
+
+                    return Page();
+                }
+                else
+                {
+                    return Redirect("/NoPermission");
+                }
+            }
+            else
+            {
+                return Redirect("/NoPermission");
+            }
+        }
+        #endregion Page Load
+
+        #region Data
+        //Method Summary:
+        //This method is excuted when the a post occurs.
+        //When excuted, it populates the page properties.
+        public async Task PopulateProperties()
+        {
+            //Get the current user ID and JWT token.
+            string? userID = _httpContextAccessor.HttpContext.User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier).Value;
+            Claim? jwtTokenClaim = _httpContextAccessor.HttpContext.User.FindFirst("Token");
+            string? jwtToken = jwtTokenClaim.Value;
+            CurrentUser = await _userService.GetUser(Convert.ToInt32(userID), jwtToken);
+
+            //Get all contractors from the DB and set in Contractors property.
+            Contractors = await _contractorService.GetContractors(jwtToken);
+            if (Contractors != null)
+            {
+                //Get the contractor details from the Contractors property by filtering on the CurrentUser property.
+                Contractor = Contractors.Where(c => c.Email == CurrentUser.Email).FirstOrDefault();
+            }
+
+            Repairs = await _repairService.GetRepairs(jwtToken);
+            Repairs = Repairs.OrderBy(r => r.RepairTargetDate).ToList();
+
+            //Filter out repairs that have been repaired.
+            Repairs = Repairs.Where(r => r.RepairStatusID != 3).ToList();
+
+            //Filter out repairs that are not assigned to the current contractor.
+            Repairs = Repairs.Where(r => r.ContractorID == Contractor.ID).ToList();
+
+            RepairStatuses = await _repairStatusService.GetRepairStatuses(jwtToken);
+            RepairStatuses = RepairStatuses.Where(rs => rs.Active == true).ToList();
+
+            RepairStatusList = RepairStatuses.Select(rs => new SelectListItem()
+            {
+                Text = rs.RepairStatusDescription,
+                Value = rs.ID.ToString()
+            });
+
+            Faults = await _faultService.GetFaults();
+
+            FaultPriorities = await _faultPriorityService.GetFaultPriorities();
+            FaultPriorities = FaultPriorities.Where(fp => fp.Active == true).ToList();
+
+            FaultStatuses = await _faultStatusService.GetFaultStatuses();
+            FaultStatuses = FaultStatuses.Where(fs => fs.Active == true).ToList();
+
+            FaultTypes = await _faultTypeService.GetFaultTypes();
+            FaultTypes = FaultTypes.Where(ft => ft.Active == true).ToList();
+        }
+
+        //Method Summary:
+        //This method is executed when a post occurs.
+        //When executed, the page properties required in the charts javascript are set in session storage.
+        public async Task SetSessionData()
+        {
+            HttpContext.Session.SetInSession("FaultTypes", FaultTypes);
+            HttpContext.Session.SetInSession("FaultPriorities", FaultPriorities);
+            HttpContext.Session.SetInSession("FaultStatuses", FaultStatuses);
+            HttpContext.Session.SetInSession("RepairStatuses", RepairStatuses);
+            HttpContext.Session.SetInSession("Contractor", Contractor);
+        }
+        #endregion Data
+
+        #region Pagination
+        //Method Summary:
+        //This method is excuted when the pagination buttons are clicked.
+        //When executed the desired page of faults is displayed.
+        public async Task OnGetPaging()
+        {
+            await PopulateProperties();
+
+            //Hide the repair map.
+            ShowRepairMap = false;
+
+            //Show the repair table.
+            ShowRepairTable = true;
+
+            //Setup Pager.
+            Pager.Count = Repairs.Count;
+            Pager.PageSize = await _settingsService.GetSettingInt(DFI.FaultReporting.Common.Constants.Settings.PAGESIZE);
+            PagedRepairs = await _pagerService.GetPaginatedRepairs(Repairs, Pager.CurrentPage, Pager.PageSize);
+            PagedRepairs = PagedRepairs.OrderBy(r => r.RepairTargetDate).ToList();
+
+            //Create a new list of AvailableJobs objects.
+            AvailableJobs = new List<SelectedJobsInputModel>();
+
+            //Loop through all repairs and add them to the AvailableJobs list.
+            foreach (Repair repair in PagedRepairs)
+            {
+                //Create a new SelectedJobsInput object.
+                SelectedJobsInput = new SelectedJobsInputModel();
+                SelectedJobsInput.RepairID = repair.ID;
+                SelectedJobsInput.isSelected = false;
+
+                //Add the SelectedJobsInput object to the AvailableJobs list.
+                AvailableJobs.Add(SelectedJobsInput);
+            }
+
+            SelectedJobs = HttpContext.Session.GetFromSession<List<SelectedJobsInputModel>>("SelectedJobs");
+
+            foreach (SelectedJobsInputModel selectedJob in SelectedJobs)
+            {
+                foreach (SelectedJobsInputModel availableJob in AvailableJobs)
+                {
+                    if (selectedJob.RepairID == availableJob.RepairID && selectedJob.isSelected)
+                    {
+                        availableJob.isSelected = true;
+                    }
+                }
+            }
+
+            HttpContext.Session.SetInSession("AvailableJobs", AvailableJobs);
+
+            TempData["CurrentPage"] = Pager.CurrentPage;
+            TempData.Keep();
+        }
+        #endregion Pagination
+
+        #region Select Jobs
+        //This method is executed when a checkbox is clicked in the jobs table.
+        //When executed the Selected Jobs list is updated.
+        public async Task<IActionResult> OnPostSelectJobs()
+        {
+            await PopulateProperties();
+
+            int CurrentPage = 1;
+
+            if (TempData["CurrentPage"] != null && (int)TempData["CurrentPage"] != 0)
+            {
+                CurrentPage = (int)TempData["CurrentPage"];
+            }
+
+            //Setup Pager.
+            Pager.Count = Repairs.Count;
+            Pager.CurrentPage = CurrentPage;
+            Pager.PageSize = await _settingsService.GetSettingInt(DFI.FaultReporting.Common.Constants.Settings.PAGESIZE);
+            PagedRepairs = await _pagerService.GetPaginatedRepairs(Repairs, Pager.CurrentPage, Pager.PageSize);
+            PagedRepairs = PagedRepairs.OrderBy(r => r.RepairTargetDate).ToList();
+
+            SelectedJobs = HttpContext.Session.GetFromSession<List<SelectedJobsInputModel>>("SelectedJobs");
+
+            if (SelectedJobs == null || SelectedJobs.Count == 0)
+            {
+                SelectedJobs = new List<SelectedJobsInputModel>();
+            }
+
+            if (AvailableJobs == null || AvailableJobs.Count == 0)
+            {
+                //Create a new list of AvailableJobs objects.
+                AvailableJobs = new List<SelectedJobsInputModel>();
+                AvailableJobs = new List<SelectedJobsInputModel>();
+
+                //Loop through all repairs and add them to the AvailableJobs list.
+                foreach (Repair repair in PagedRepairs)
+                {
+                    //Create a new SelectedJobsInput object.
+                    SelectedJobsInput = new SelectedJobsInputModel();
+                    SelectedJobsInput.RepairID = repair.ID;
+                    SelectedJobsInput.isSelected = false;
+
+                    //Add the SelectedJobsInput object to the AvailableJobs list.
+                    AvailableJobs.Add(SelectedJobsInput);
+                }
+            }
+
+            foreach (SelectedJobsInputModel availableJob in AvailableJobs)
+            {
+                if (availableJob.isSelected)
+                {
+                    SelectedJobsInputModel jobToAdd = SelectedJobs.Where(sj => sj.RepairID == availableJob.RepairID).FirstOrDefault();
+
+                    if (jobToAdd == null)
+                    {
+                        SelectedJobsInputModel selectedJob = new SelectedJobsInputModel();
+                        selectedJob.RepairID = availableJob.RepairID;
+                        selectedJob.isSelected = true;
+
+                        SelectedJobs.Add(selectedJob);
+                    }
+                }
+                else
+                {
+                    SelectedJobsInputModel jobToRemove = SelectedJobs.Where(sj => sj.RepairID == availableJob.RepairID).FirstOrDefault();
+
+                    if (jobToRemove != null)
+                    {
+                        SelectedJobs.Remove(jobToRemove);
+                    }
+                }
+            }
+
+            foreach (SelectedJobsInputModel selectedJob in SelectedJobs)
+            {
+                foreach (SelectedJobsInputModel availableJob in AvailableJobs)
+                {
+                    if (selectedJob.RepairID == availableJob.RepairID && selectedJob.isSelected)
+                    {
+                        availableJob.isSelected = true;
+                    }
+                }
+            }
+
+            HttpContext.Session.SetInSession("AvailableJobs", AvailableJobs);
+            HttpContext.Session.SetInSession("SelectedJobs", SelectedJobs);
+
+            TempData.Keep();
+
+            return Page();
+        }
+        #endregion Select Jobs
+
+        #region Plot Routes
+        //Method Summary:
+        //This method is excuted when the plot route button is clicked.
+        //When executed the repairs are sorted by priority, the map is shown and the route is plotted.
+        public async Task OnGetPlotRoute()
+        {
+            ShowRouteMap = true;
+
+            await PopulateProperties();
+
+            int CurrentPage = 1;
+
+            if (TempData["CurrentPage"] != null && (int)TempData["CurrentPage"] != 0)
+            {
+                CurrentPage = (int)TempData["CurrentPage"];
+            }
+
+            //Setup Pager.
+            Pager.Count = Repairs.Count;
+            Pager.CurrentPage = CurrentPage;
+            Pager.PageSize = await _settingsService.GetSettingInt(DFI.FaultReporting.Common.Constants.Settings.PAGESIZE);
+            PagedRepairs = await _pagerService.GetPaginatedRepairs(Repairs, Pager.CurrentPage, Pager.PageSize);
+            PagedRepairs = PagedRepairs.OrderBy(r => r.RepairTargetDate).ToList();
+
+            SelectedJobs = HttpContext.Session.GetFromSession<List<SelectedJobsInputModel>>("SelectedJobs");
+
+            List<Repair> SelectedRepairs = new List<Repair>();
+
+            foreach (SelectedJobsInputModel selectedJob in SelectedJobs)
+            {
+                foreach (Repair repair in Repairs)
+                {
+                    if (selectedJob.RepairID == repair.ID)
+                    {
+                        SelectedRepairs.Add(repair);
+                    }
+                }
+            }
+
+            List<Fault> selectedFaults = new List<Fault>();
+
+            foreach (Fault fault in Faults)
+            {
+                foreach (Repair repair in SelectedRepairs)
+                {
+                    if (fault.ID == repair.FaultID)
+                    {
+                        selectedFaults.Add(fault);
+                    }
+                }
+            }
+
+            selectedFaults = selectedFaults.OrderBy(f => f.FaultPriorityID).ToList();
+
+            HttpContext.Session.SetInSession("SelectedRepairs", SelectedRepairs);
+            HttpContext.Session.SetInSession("SelectedFaults", selectedFaults);
+
+            await SetSessionData();
+        }
+        #endregion Plot Routes
     }
 }
---
+++ DFI.FaultReporting.Public/wwwroot/js/mapping/PlotJobRoute.js
@@ -0,0 +1,182 @@
+﻿//Azure Maps Subscription Key - Needed to access the service
+const azureMapsSubscriptionKey = "tVwRA8vhqB9AvHiYgZa1muR90phLPrp6qzmJFvjqa0Q";
+
+var restRoutingRequestUrl = 'https://{azMapsDomain}/route/directions/json?api-version=1.0&query={query}&routeRepresentation=polyline&travelMode=car&view=Auto';
+
+//Global variables used throughout code
+var map, marker, searchURL, source, popup, routePointQuery, routeLine, defaultOrder;
+
+var routePoints = [];
+
+//Define map boundaries - this will restrict how far a user can pan on the map control and also the search results returned in the autocomplete
+const mapBounds = [-8.3, 53.9, -5.3, 55.4]
+
+//FUNCTION SUMMARY:
+//This method is used for setting up the map control and adding event listeners to the map - uses the Azure Maps Web SDK.
+//In this instance a data source for displaying reapirs is created and added to the map also.
+function initRouteMap() {
+    //Find map on page
+    var routeMap = document.getElementById('routeMap');
+
+    if (routeMap != null) {
+        //Create new map control
+        map = new atlas.Map('routeMap', {
+            //Center map on Northern Ireland
+            center: [-6.8, 54.65],
+            //Set zoom to 3 to show as much of Northern Ireland as possible
+            zoom: 3,
+            //Add boundaries to restrict how user can pan and zoom the map control - needed to stop user panning or zooming to a location outside of Northern Ireland
+            maxBounds: mapBounds,
+            //Set lanuage to GB for better localisation
+            language: 'en-GB',
+            //Set auth
+            authOptions: {
+                authType: 'subscriptionKey',
+                subscriptionKey: azureMapsSubscriptionKey
+            },
+            style: 'road'
+        });
+
+        //Add full screen control to the map.
+        map.controls.add(new atlas.control.FullscreenControl(), {
+            position: 'bottom-right',
+            style: 'auto'
+        });
+
+        //Add zoom control to the map.
+        map.controls.add(new atlas.control.ZoomControl(), {
+            position: 'bottom-right'
+        });
+
+        //Add the style control to the map.
+        map.controls.add(new atlas.control.StyleControl(), {
+            position: 'bottom-right',
+            mapStyles: 'all'
+        });
+
+        faults.forEach(function (fault) {
+            position = []
+            position[0] = fault.longitude;
+            position[1] = fault.latitude;
+            routePoints.push(position)
+        });
+
+        navigator.geolocation.getCurrentPosition(SetStartPosition);
+
+        //Wait until the map resources are ready.
+        map.events.add('ready', function () {
+
+            //Create a data source to store the data in.
+            datasource = new atlas.source.DataSource();
+            map.sources.add(datasource);
+
+            //Add layers fro rendering data in the data source.
+            map.layers.add([
+                //Render linestring data using a line layer.
+                new atlas.layer.LineLayer(datasource, null, {
+                    strokeColor: 'red',
+                    strokeWidth: 5
+                }),
+
+                //Render point data using a symbol layer.
+                new atlas.layer.SymbolLayer(datasource, null, {
+                    textOptions: {
+                        textField: ['get', 'title'],
+                        offset: [0, -1.2],
+                        color: 'white'
+                    },
+                    filter: ['any', ['==', ['geometry-type'], 'Point'], ['==', ['geometry-type'], 'MultiPoint']] //Only render Point or MultiPoints in this layer.
+                })
+            ]);
+
+            //Add routePoints to the map and build the waypoint query string.
+            var points = [];
+            var query = [];
+            defaultOrder = [];
+
+            for (var i = 0; i < routePoints.length; i++) {
+                points.push(new atlas.data.Feature(new atlas.data.Point(routePoints[i]), {
+                    title: i + ''
+                }));
+
+                query.push(routePoints[i][1] + ',' + routePoints[i][0]);
+                defaultOrder.push(i);
+            }
+
+            //Add the points to the data source.
+            datasource.add(points);
+
+            //Create the waypoint query string value to be used in the routing requests.
+            routePointQuery = query.join(':');
+        });
+
+    }
+}
+
+function SetStartPosition(position) {
+
+    var startPosition = [];
+    startPosition[0] = position.coords.longitude;
+    startPosition[1] = position.coords.latitude;
+
+    routePoints.push(startPosition)
+}
+
+function GenerateRoute(optimized) {
+
+    //Remove any previously calculated route information.
+    if (routeLine) {
+        datasource.remove(routeLine);
+        routeLine = null;
+    }
+
+    //Create request to calculate a route in the order in which the routePoints are provided.
+    var requestUrl = restRoutingRequestUrl.replace('{query}', routePointQuery);
+
+    if (optimized) {
+        requestUrl += '&computeBestOrder=true';
+    }
+
+    //Proces the request.
+    processRequest(requestUrl).then(r => {
+        PlotRoute(r.routes[0]);
+
+        var output = 'Distance: ' + Math.round(r.routes[0].summary.lengthInMeters / 10) / 100 + ' km<br/>';
+
+        if (optimized) {
+            var pinOrder = ['0'];
+
+            for (var i = 0; i < r.optimizedWaypoints.length; i++) {
+                //Increase index by one to account for origin index.
+                pinOrder.push(r.optimizedWaypoints[i].optimizedIndex + 1);
+            }
+
+            //Add the desintation index to the end.
+            pinOrder.push(routePoints.length - 1);
+
+            output += 'Waypoint Order: ' + pinOrder.join(', ');
+        } else {
+            output += 'Waypoint Order: ' + defaultOrder.join(', ');
+        }
+    });
+}
+
+function PlotRoute(route) {
+    var routeCoordinates = [];
+
+    for (var legIndex = 0; legIndex < route.legs.length; legIndex++) {
+        var leg = route.legs[legIndex];
+
+        //Convert the route point data into a format that the map control understands.
+        var legCoordinates = leg.points.map(function (point) {
+            return [point.longitude, point.latitude];
+        });
+
+        //Combine the route point data for each route leg together to form a single path.
+        routeCoordinates = routeCoordinates.concat(legCoordinates);
+    }
+
+    //Create a LineString from the route path points and add it to the data source.
+    routeLine = new atlas.Shape(new atlas.data.LineString(routeCoordinates));
+    datasource.add(routeLine);
+}
