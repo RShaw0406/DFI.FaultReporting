08732d9fb5e9c281ad659bcbca5957055eb50ebd
Ryan Shaw <Shaw-R7@ulster.ac.uk>
2024-07-16 00:15:01 +0100

    ClaimsMenu added to Public app

---
+++ DFI.FaultReporting.Public/Pages/Claims/ClaimsMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DFI.FaultReporting.Public.Pages.Claims
+{
+    public class ClaimsMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
